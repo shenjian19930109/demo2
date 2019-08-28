e3b3ebd309e52517bf9998ad25fd659e523e880a
shenjian <shenjian@corp.netease.com>
2019-08-29 01:01:53 +0800

    hotfix

--- src/main/java/com/chinamobile/demo/controller/DemoController.java
+++ src/main/java/com/chinamobile/demo/controller/DemoController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/demo")
 public class DemoController {
-
+    // hotfix
     @RequestMapping("/hello")
     public String hello() {
         System.out.println("hello world");
