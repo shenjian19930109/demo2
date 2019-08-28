0ab6d68fcc80c4a67d5c9b26b154bba65039d665
shenjian <shenjian@corp.netease.com>
2019-08-29 00:37:46 +0800

    first commit

---
+++ src/main/java/com/chinamobile/demo/controller/DemoController.java
@@ -0,0 +1,21 @@
+package com.chinamobile.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description
+ * @Author shenjian
+ * @Date 2019-08-21 23:27
+ **/
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        System.out.println("hello world");
+        return "world";
+    }
+}
