02644539458ca3655b99a8360940503fae398e1c
shenjian <shenjian@corp.netease.com>
2019-08-29 00:57:17 +0800

    feature

--- src/main/java/com/chinamobile/demo/controller/DemoController.java
+++ src/main/java/com/chinamobile/demo/controller/DemoController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     /**
-     *  feature hotfix
+     * feature hotfix
      * */
     @RequestMapping("/hello")
     public String hello() {
